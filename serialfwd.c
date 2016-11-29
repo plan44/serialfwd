7fadafbe661c204817dd236c64ebe52be4ccb145
Lukas Zeller <luz@plan44.ch>
2016-11-29 18:10:43 +0100

    Added forgotten 110 baud rate

--- serialfwd.c
+++ serialfwd.c
@@ -327,6 +327,7 @@ int main(int argc, char **argv)
     switch (baudRate) {
       case 50 : baudRateCode = B50; break;
       case 75 : baudRateCode = B75; break;
+      case 110 : baudRateCode = B110; break;
       case 134 : baudRateCode = B134; break;
       case 150 : baudRateCode = B150; break;
       case 200 : baudRateCode = B200; break;
